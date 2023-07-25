1c4cc8b750b5258eee0983334e00e820db888594
MariusAskelovic <marashz@gmail.com>
2023-07-25 09:47:30 +0300

    render

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-'use strict';
+import { crEl } from './helper/crEl.js';
 console.log('app.js file was loaded');
+
+const els = {
+  postContainer: document.getElementById('posts'),
+};
 
 const url = 'https://dummyjson.com/posts';
 
@@ -12,19 +16,51 @@ const url = 'https://dummyjson.com/posts';
 let mainPostArr = [];
 
 getPosts();
-render();
 
 function getPosts() {
   fetch(url + '?limit=10')
     .then((resp) => resp.json())
     .then((atsObj) => {
       mainPostArr = atsObj.posts;
-      render(mainPostArr);
+      render();
     })
     .catch(console.warn());
 }
 
 function render() {
   // spausdindami gautume parsiustus duomenis
-  console.log('mainPostArr ===', mainPostArr);
+  els.postContainer.innerHTML = '';
+  const htmlElArr = mainPostArr.map((pObj) => makeOnePostEl(pObj));
+  console.log('htmlElArr ===', htmlElArr);
+  els.postContainer.append(...htmlElArr);
+}
+
+/*
+const singlePost = {
+  id: 1,
+  title: 'His mother had always taught him',
+  body: 'His mother',
+  userId: 9,
+  tags: ['history', 'american', 'crime'],
+  reactions: 2,
+};
+to ========
+<li class="post">
+    <h3>title</h3>
+    <p>intro</p>
+    <a class="post-link" href="#">read more...</a>
+</li>
+*/
+
+function makeOnePostEl(pObj) {
+  const liEl = crEl('li', { class: 'post' });
+  const titleEl = crEl('h3', {}, pObj.title);
+  const pEl = crEl('p', {}, pObj.body.slice(0, 25) + '...');
+  const linkEl = crEl(
+    'a',
+    { class: 'post-link', href: 'single-post.html' },
+    'read more...'
+  );
+  liEl.append(titleEl, pEl, linkEl);
+  return liEl;
 }
